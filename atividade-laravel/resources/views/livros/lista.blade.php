3be49fbe4b7df206b8ec5026f55efe88c164e496
Henrique Sales <henriquesabsales912@gmail.com>
2025-05-07 10:02:02 -0300

    add edit/delete

--- atividade-laravel/resources/views/livros/lista.blade.php
+++ atividade-laravel/resources/views/livros/lista.blade.php
@@ -8,18 +8,33 @@
             <thead>
                 <tr>
                     <th>ID</th>
+                    <th>Título</th>
                     <th>Autor</th>
                     <th>Ano</th>
                     <th>Descrição</th>
+                    <th>Ações</th>
                 </tr>
             </thead>
             <tbody>
                 @foreach ($livros as $livro)
                     <tr>
                         <td>{{ $livro->id }}</td>
+                        <td>{{ $livro->titulo }}</td>
                         <td>{{ $livro->autor }}</td>
                         <td>{{ $livro->ano_publicacao}}</td>
                         <td>{{ $livro->descricao }}</td>
+                        <td>
+                            <form action="/livros/editar" method="POST">
+                                @csrf
+                                <input type="hidden" name="id" value="{{ $livro->id }}">
+                                <button type="submit">Editar</button>
+                            </form>
+                            <form action="/livros/excluir" method="POST">
+                                @csrf
+                                <input type="hidden" name="id" value="{{ $livro->id }}">
+                                <button type="submit">Excluir</button>
+                            </form>
+                        </td>
                     </tr>
                 @endforeach
             </tbody>
