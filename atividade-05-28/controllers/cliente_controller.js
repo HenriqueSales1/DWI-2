87b04bd990f240d35e625579d00978bfedea02eb
Henrique Sales <henriquesabsales912@gmail.com>
2025-05-28 10:02:21 -0300

    adc metodos de edição/exclusao

--- atividade-05-28/controllers/cliente_controller.js
+++ atividade-05-28/controllers/cliente_controller.js
@@ -1,17 +1,51 @@
-import Cliente from '../models/cliente.js';
+import Cliente from "../models/cliente.js";
 
 async function criarCliente(req, res) {
-    const cliente = await Cliente.create({
-        nome: req.body.nome,
-        email: req.body.email,
-        telefone: req.body.telefone
-    });
-    res.json(cliente);
+  const cliente = await Cliente.create({
+    nome: req.body.nome,
+    email: req.body.email,
+    telefone: req.body.telefone,
+  });
+  res.json(cliente);
 }
 
 async function listarClientes(req, res) {
-    const clientes = await Cliente.findAll();
-    res.json(clientes);
+  const clientes = await Cliente.findAll();
+  res.json(clientes);
 }
 
-export {criarCliente, listarClientes};
+async function editarCliente(req, res) {
+  const cliente = await Cliente.findOne({
+    where: {
+      id: req.body.id,
+    },
+  });
+  if (cliente) {
+    cliente.nome = req.body.nome;
+    cliente.email = req.body.email;
+    cliente.telefone = req.body.telefone;
+    if (await cliente.save()) {
+      res.json(cliente);
+    } else {
+      res.status(500).json({ error: "Erro ao editar cliente" });
+    }
+  } else {
+    res.status(404).json({ error: "Cliente não encontrado" });
+  }
+}
+
+async function excluirCliente(req, res) {
+  const cliente = await Cliente.findOne({
+    where: {
+      id: req.body.id,
+    },
+  });
+  if (cliente) {
+    await cliente.destroy();
+    res.json({ message: "Cliente excluído com sucesso" });
+  } else {
+    res.status(404).json({ error: "Cliente não encontrado" });
+  }
+}
+
+export { criarCliente, listarClientes, editarCliente, excluirCliente };
--- atividade-05-28/routes/cliente_router.js
+++ atividade-05-28/routes/cliente_router.js
@@ -1,8 +1,10 @@
-import { criarCliente, listarClientes } from "../controllers/cliente_controller.js";
+import { criarCliente, listarClientes, editarCliente, excluirCliente } from "../controllers/cliente_controller.js";
 import { Router } from "express";
 
 const clienteRouter = Router();
 clienteRouter.post("/", criarCliente);
 clienteRouter.get("/", listarClientes);
+clienteRouter.put("/", editarCliente);
+clienteRouter.delete("/", excluirCliente);
 
 export default clienteRouter;
