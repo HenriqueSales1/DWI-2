d8df9bfd326b9a8bba3468a9fbfb24cebd0a055b
Henrique Sales <henriquesabsales912@gmail.com>
2025-06-25 09:43:45 -0300

    mod handlebars + add table users/login

--- atividade-05-28/app.js
+++ atividade-05-28/app.js
@@ -1,15 +1,21 @@
 import express from "express";
-import {create} from "express-handlebars";
+import { create } from "express-handlebars";
+import session from "express-session";
+import css from "connect-session-sequelize";
 
 import film_web_router from "./routes/web/film_router.js";
 import cliente_web_router from "./routes/web/cliente_router.js";
 import dependente_web_router from "./routes/web/dependente_router.js";
 import game_web_router from "./routes/web/game_router.js";
+import user_web_router from "./routes/web/user_router.js";
 import syncer from "./database/syncer.js";
+import sequelize from "./database/mysql.js";
+import { checkLogged } from "./controllers/web/user_controller.js";
 
-if (!await syncer()) {
+if (!(await syncer())) {
   process.exit();
 }
+
 const app = express();
 
 const hbs = create({
@@ -18,6 +24,42 @@ const hbs = create({
   layoutsDir: "views/layouts",
   partialsDir: "views/partials",
 });
+
+hbs.handlebars.registerHelper("eq", function (arg1, arg2) {
+  return arg1 === arg2;
+});
+
+hbs.handlebars.registerHelper("inc", function (arg1, arg2) {
+  if (typeof arg1 == "undefined") {
+    return false;
+  }
+  return arg1.indexOf(arg2) !== -1;
+});
+
+const SequelizeStore = css(session.Store);
+const sequelizeStore = new SequelizeStore({
+  db: sequelize,
+  tableName: "sessions",
+  checkExpirationInterval: 5 * 60 * 1000,
+  expiration: 1 * 60 * 60 * 1000, // 1 hour
+});
+
+app.use(
+  session({
+    secret: '*&long+and+secure+secret=%445',
+    name: "sess_cookie_param",
+    store: sequelizeStore,
+    cookie: {
+      maxAge: 1 * 60 * 60 * 1000, // 1 hour
+      secure: false, // true if using https
+      httpOnly: true,
+    },
+    saveUninitialized: false,
+    resave: false,
+  })
+)
+
+await sequelizeStore.sync();
 
 app.use(express.json());
 app.use(express.urlencoded());
@@ -34,6 +76,7 @@ app.use("/films", film_web_router);
 app.use("/clientes", cliente_web_router);
 app.use("/dependentes", dependente_web_router);
 app.use("/games", game_web_router);
+app.use("/users", user_web_router);
 
 app.listen(80, () => {
   console.log("Servidor rodando na porta 80");
---
+++ atividade-05-28/routes/web/user_router.js
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import {
+  promptUser,
+  createUser,
+  promptLogin,
+  login,
+  logout,
+  checkLogged,
+} from "../../controllers/web/user_controller.js";
+
+const user_web_router = Router();
+user_web_router.get("/register", promptUser);
+user_web_router.post("/register", createUser);
+user_web_router.get("/login", promptLogin);
+user_web_router.post("/login", login);
+user_web_router.get("/logout", logout);
+
+export default user_web_router;
