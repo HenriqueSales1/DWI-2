ae82c000069fe5d0eeef8f3479b22d614357b9cb
Henrique Sales <henriquesabsales912@gmail.com>
2025-06-27 07:23:21 -0300

    att sequelize para teste de deploy

--- atividade-05-28/database/mysql.js
+++ atividade-05-28/database/mysql.js
@@ -1,13 +1,14 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize({
-  dialect: "mysql",
-  host: "localhost",
-  port: 3306,
-  username: "root",
-  password: "root",
-  database: "atividade_05_28",
-});
+// const sequelize = new Sequelize({
+//   dialect: "mysql",
+//   host: "localhost",
+//   port: 3306,
+//   username: "root",
+//   password: "root",
+//   database: "atividade_05_28",
+// });
 
+const sequelize = new Sequelize("postgresql://user_teste_deploy:FrJge8todZ84cvms4ipij3wiDD7rNC6u@dpg-d1f71eur433s7399g41g-a/db_teste_deploy_x53y")
 
 export default sequelize;
